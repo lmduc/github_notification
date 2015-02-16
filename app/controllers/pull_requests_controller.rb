c59ef528ac480e0fe10ffe4117829a23e4daedad
DUC LE <ducle@DUCs-MacBook-Pro-2.local>
2015-02-16 19:13:12 +0700

    Refactor code

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -2,7 +2,13 @@ class PullRequestsController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: :event
 
   def event
-    PullRequest.update_from_github(params[:pull_request])
+    PullRequest.update_from_github(pull_request_params)
     render nothing: true
+  end
+
+  private
+
+  def pull_request_params
+    params[:pull_request]
   end
 end
--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -9,9 +9,8 @@ class PullRequest < ActiveRecord::Base
   class << self
     def update_from_github(pull_request_attrs)
       pull_request = PullRequest.find_or_initialize_by(pull_request_id: pull_request_attrs[:id])
-      pull_request.assign_attributes(data: pull_request_attrs,
+      pull_request.update_attributes(data: pull_request_attrs,
                                      status: pull_request_attrs[:state])
-      pull_request.save
       pull_request
     end
   end
